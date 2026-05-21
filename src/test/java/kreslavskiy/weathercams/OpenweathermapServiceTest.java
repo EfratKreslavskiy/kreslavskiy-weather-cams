bc737e77f734a90a517c9f2dc7944260c8f14bc6
Efrat Kreslavskiy <ekreslav@student.touro.edu>
2026-05-21 02:14:31 -0400

    added openweathermapServiceTest

---
+++ src/test/java/kreslavskiy/weathercams/OpenweathermapServiceTest.java
@@ -0,0 +1,30 @@
+package kreslavskiy.weathercams;
+
+import com.andrewoid.apikeys.ApiKey;
+import io.reactivex.rxjava3.annotations.NonNull;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpenweathermapServiceTest
+{
+    @Test
+    void getLocation()
+    {
+        //given
+        OpenweathermapService owmService = new OpenweathermapServiceFactory().create();
+
+        //when
+        ApiKey apiKey = new ApiKey("openweathermapKey");
+        String keyString = apiKey.get();
+
+        List<Location> locations = owmService.getLocation(
+                         "Passaic", 5, keyString).blockingGet();
+
+        //then
+        assertEquals("40.8566936", locations.get(0).lat());
+        assertEquals("-74.1286298", locations.get(0).lon());
+    }
+}
